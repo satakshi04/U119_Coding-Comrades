379fa5a1e5fbcc97abe75a99646a65882dc20f29
shreya <rshreyaiyer2004@gmail.com>
2024-09-26 18:19:31 +0530

    initial

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to show contact form when the contact info input is clicked
+function showContactForm() {
+    var contactForm = document.getElementById("contact-form");
+    contactForm.classList.remove("hidden");
+}
