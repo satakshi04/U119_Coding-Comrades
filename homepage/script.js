310b30848d9f2cec7e7dce9bfd4b5a7d1532059f
Hk-0308 <hr9218@srmist.edu.in>
2024-09-27 10:17:19 +0530

    homepage harish

---
+++ homepage/script.js
@@ -0,0 +1,7 @@
+function goToConduct() {
+    window.location.href = 'admin.html';  // Redirect to admin page
+}
+
+function goToEvent() {
+    window.location.href = 'participant.html';  // Redirect to participant page
+}
